7d7ce6c404a2d72e9f0419945ab42327c4a67356
Sir-Helloze <babywalk@mail.ru>
2022-05-20 13:55:21 +0400

    Create add.php

---
+++ phpCode/add.php
@@ -0,0 +1,19 @@
+<?
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+$connect = mysqli_connect('localhost','root','','library');
+$title = $_POST['title'];
+$auther = $_POST['auther'];
+$content = $_POST['content'];
+$sql = "INSERT INTO `books1` (`title`, `auther`,`content`) VALUES ('$title', '$auther','$content ');";
+if ($title && $content && $auther) {
+  $run_sql = mysqli_query($connect,$sql);
+  if ($run_sql) {
+    echo '<p style="color:#008000;">Вы успешно добавили книгу!</p>';
+  }
+}
+else
+{
+  echo '<p style="color:red;">Заполните поля!</p>';
+}
+}
